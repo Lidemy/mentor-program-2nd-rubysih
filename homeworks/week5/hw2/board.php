5c96c842fdb9d9a344286d054c7e6f6b64dde3d9
Ruby Sih <ruby.sih@cloudysys.com>
2018-10-03 17:59:01 +0800

    1.messages order 2.set pages

--- homeworks/week5/hw2/board.php
+++ homeworks/week5/hw2/board.php
@@ -62,64 +62,107 @@
             </form>
         </div>
         <?php   //撈取留言紀錄
-            $sql = "SELECT * FROM message_board WHERE parent = 0";
+            $pageSize = 10; //一頁幾筆資料
+            $pageSum = 1;  //總共幾頁
+            if(isset($_GET['pageNow'])){    //設定目前頁數
+                $pageNow = $_GET['pageNow'];   
+            }else{
+                $pageNow=1;
+            }
+            
+            $sql = "SELECT * FROM message_board WHERE parent = 0 ORDER BY date DESC";
             $result = $conn->query($sql);
             
             if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo
-                    '<div class="message__block">
-                        <div class="info">
-                            <p class="name">' 
-                                . $row["nickname"]
-                            .'</p>
-                            <p class="time">'
-                                . $row["date"]
-                            .'</p>
-                        </div>
-                        <div class="content">'
-                            . $row["content"]
-                        .'</div>
-                        <div class="reply__block">
-                    ';
-                    //撈取子留言
-                    $sql_child = "SELECT * FROM message_board WHERE parent = " . $row["id"];
-                    $result_child = $conn->query($sql_child);
-                    if ($result_child->num_rows > 0) {
-                        while($row_child = $result_child->fetch_assoc()) {
-                            echo 
-                            '<div class="info">
-                                <p class="name">'
-                                    . $row_child["nickname"]
+                $nowNum=$result->num_rows;  //留言總數
+                $pageSum = ceil($result->num_rows /10);
+
+                for($i=1;$i<=$nowNum;$i++){
+                    $row = $result->fetch_assoc();
+                    if(($pageNow-1)*10<$i && $i<=setPage($pageNow,$nowNum)){
+
+                        echo
+                        '<div class="message__block">
+                            <div class="info">
+                                <p class="name">' 
+                                    . $row["nickname"]
                                 .'</p>
                                 <p class="time">'
-                                    . $row_child["date"]
+                                    . $row["date"]
                                 .'</p>
                             </div>
                             <div class="content">'
-                                . $row_child["content"]
+                                . $row["content"]
                             .'</div>
-                            ';
+                            <div class="reply__block">
+                        ';
+                        //撈取子留言
+                        $sql_child = "SELECT * FROM message_board WHERE parent = " . $row["id"] . " ORDER BY date DESC";
+                        $result_child = $conn->query($sql_child);
+                        if ($result_child->num_rows > 0) {
+                            while($row_child = $result_child->fetch_assoc()) {
+                                echo 
+                                '<div class="info">
+                                    <p class="name">'
+                                        . $row_child["nickname"]
+                                    .'</p>
+                                    <p class="time">'
+                                        . $row_child["date"]
+                                    .'</p>
+                                </div>
+                                <div class="content">'
+                                    . $row_child["content"]
+                                .'</div>
+                                ';
+                            }
                         }
-                    }
-                    //子留言-新增留言
-                    echo 
-                            '<div class="new__message">
-                                <form action="/board/new_message.php" method="POST">
-                                    暱稱 : <input type="text" name="nickname"';
-                                    //有登入就代入nickname
-                                    if(isset($nickname)){echo 'value="'.$nickname.'" ';}
-                                    echo ' readonly>
-                                    內容 : <textarea name="content" id="content" cols="30" rows="5"></textarea>
-                                    <input type="text" name="parent" value='.$row["id"].' class="display__none">
-                                    <input type="submit" value="送出" class="btn btn__submit">
-                                </form>
+                        //子留言-新增留言
+                        echo 
+                                '<div class="new__message">
+                                    <form action="/board/new_message.php" method="POST">
+                                        暱稱 : <input type="text" name="nickname"';
+                                        //有登入就代入nickname
+                                        if(isset($nickname)){echo 'value="'.$nickname.'" ';}
+                                        echo ' readonly>
+                                        內容 : <textarea name="content" id="content" cols="30" rows="5"></textarea>
+                                        <input type="text" name="parent" value='.$row["id"].' class="display__none">
+                                        <input type="submit" value="送出" class="btn btn__submit">
+                                    </form>
+                                </div>
                             </div>
-                        </div>
-                    </div>';
+                        </div>';
+                    }
                 }
-            } 
+            }
         ?>
+        <div class="page">
+            <?php
+                for($i=1;$i<=$pageSum;$i++){
+                    echo 
+                    '<span class="page_'.$i.'">
+                        <a href="board.php?pageNow='.$i.'">'.$i.'</a>
+                    </span>';
+                }
+            ?>
+        </div>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            console.log(<?php echo $pageNow;?>);
+            nowPage(<?php echo $pageNow;?>);
+        })
+        function nowPage(pageNow){
+            document.querySelector('.page_'+pageNow).className+=' now';
+        }
+    </script>
 </body>
 </html>
+<?php
+    function setPage($pageNow,$nowNum){
+        if($pageNow*10>$nowNum){
+            return $nowNum;
+        }else{
+            return $pageNow*10;
+        }
+    }
+?>
