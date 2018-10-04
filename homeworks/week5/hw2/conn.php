76869f3298186401287a39073980b60826692b69
Ruby Sih <ruby.sih@cloudysys.com>
2018-10-04 15:26:04 +0800

    set time zone

--- homeworks/week5/hw2/conn.php
+++ homeworks/week5/hw2/conn.php
@@ -7,6 +7,7 @@
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     $conn->query("SET NAMES 'UTF8'");
+    $conn->query("set time_zone = '+8:00'");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
